82df9a8136525202c239306aa7fd1017c61ec16a
data-analysis-bi <Taralpatel.ca06@gmail.com>
2026-02-03 22:33:30 -0500

    Initialize Electron main process with window setup

--- electron/main.js
+++ electron/main.js
@@ -1,1 +1,24 @@
-// Electron main process
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  } else {
+    win.loadURL("http://localhost:5173");
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});// Electron main process
